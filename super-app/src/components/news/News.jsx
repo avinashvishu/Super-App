94d16887c13f268d9adad6a31c0526a85a2b4998
V Avinash <loginavi1@gmail.com>
2023-12-10 14:58:12 +0530

    added timer card

---
+++ super-app/src/components/news/News.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './news.module.css'
+const News = () => {
+  return (
+    <div className={styles.container} >News</div>
+  )
+}
+
+export default News
--- super-app/src/pages/DashBoard/DashBoard.jsx
+++ super-app/src/pages/DashBoard/DashBoard.jsx
@@ -3,15 +3,24 @@ import styles from './DashBoard.module.css'
 import UserInfo from '../../components/UserInfo/UserInfo'
 import Notes from '../../components/Notes/Notes'
 import Weather from '../../components/weather/weather'
+import Timer from '../../components/Timmer/Timer'
+import News from '../../components/news/News'
 
 const DashBoard = () => {
-  return (
+  return (<>
+    <div className={styles.outerGrid}>
     <div className={styles.gridlayout}>
       <div className={styles.UserInfoWrap} ><UserInfo/></div>  
  
       <div className={styles.notes} ><Notes /></div>
       <div className={styles.weather} > <Weather/> </div>
     </div>
+    <div className={styles.NewsSec} ><News/></div>
+    <div className={styles.timmerSec} > <Timer /> </div>
+
+    </div>
+    <button className={styles.button} >Browser</button>
+    </>
   )               
 }
 
